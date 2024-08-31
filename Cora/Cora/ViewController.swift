4d5aa98acaccff7e2ce1da75ae7a58f06122bccd
Fabrício Rodrigues Pujol <fabriciopujol@MacBook-Pro-de-Fabricio-2.local>
2024-08-31 16:26:36 -0300

    Project Creation.

---
+++ Cora/Cora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cora
+//
+//  Created by Fabricio Pujol on 31/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
