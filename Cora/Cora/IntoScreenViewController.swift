e5cc128f73cba69a2a08640d2635e3a495ada25c
Fabrício Rodrigues Pujol <fabriciopujol@MacBook-Pro-de-Fabricio-2.local>
2024-08-31 16:36:23 -0300

    Project Creation.

---
+++ Cora/Cora/IntoScreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  IntoScreenViewController.swift
+//  Cora
+//
+//  Created by Fabricio Pujol on 31/08/24.
+//
+
+import UIKit
+
+class IntoScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+}
